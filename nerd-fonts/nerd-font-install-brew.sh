1d501ee7b39fc1ad141b3a7d29625f42dd06ec81
fbiernat <filipbiernat@gmail.com>
2022-12-27 23:39:30 +0100

    Dotfiles update

---
+++ nerd-fonts/nerd-font-install-brew.sh
@@ -0,0 +1,28 @@
+brew tap homebrew/cask-fonts
+brew install --cask font-droid-sans-mono-nerd-font
+brew install --cask font-fantasque-sans-mono-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-heavy-data-nerd-font
+brew install --cask font-inconsolata-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-lekton-nerd-font 
+brew install --cask font-monoid-nerd-font 
+brew install --cask font-mononoki-nerd-font
+brew install --cask font-roboto-mono-nerd-font
+brew install --cask font-shure-tech-mono-nerd-font
+brew install --cask font-terminess-ttf-nerd-font
+
+# My favourite nerd-fonts
+# droid sans mono
+# fantasque sans mono
+# hack
+# heavydata
+# inconsolata
+# jetbrains mono
+# lekton
+# lilex 
+# monoid
+# mononoki
+# roboto mono
+# shure tech mono
+# terminess

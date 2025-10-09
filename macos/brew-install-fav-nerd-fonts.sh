0224c4f3dd5c933496f0ff0aa6c7ed3e4bf7686f
philbjern <filipbiernat@gmail.com>
2025-10-09 12:01:38 +0200

    fix: nerd fonts list

--- macos/brew-install-fav-nerd-fonts.sh
+++ macos/brew-install-fav-nerd-fonts.sh
@@ -1,33 +1,8 @@
+#!/bin/zsh
 brew install --cask font-menlo-for-powerline
 brew install --cask font-ibm-plex-mono
 brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-powerline-symbols
 brew install --cask font-sf-mono-for-powerline
-brew install --cask font-source-code-pro-forsource-code-pro-for-powerline
-
-# brew tap homebrew/cask-fonts
-# brew install --cask font-droid-sans-mono-nerd-font
-# brew install --cask font-fantasque-sans-mono-nerd-font
-# brew install --cask font-heavy-data-nerd-font
-# brew install --cask font-lekton-nerd-font 
-# brew install --cask font-monoid-nerd-font 
-# brew install --cask font-mononoki-nerd-font
-# brew install --cask font-roboto-mono-nerd-font
-# brew install --cask font-shure-tech-mono-nerd-font
-# brew install --cask font-terminess-ttf-nerd-font
-
-# My favourite nerd-fonts
-# droid sans mono
-# fantasque sans mono
-# hack
-# heavydata
-# inconsolata
-# jetbrains mono
-# lekton
-# lilex 
-# monoid
-# mononoki
-# roboto mono
-# shure tech mono
-# terminess
+brew install --cask font-source-code-pro-for-powerline
